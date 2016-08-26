55e939d9fc60b30e8f33ec4a6fc6786d44cc9f42
choiwj1012 <choiwj1012@hanmail.net>
2016-08-26 09:29:24 +0900

    main update

---
+++ src/shoppingMall/mainController/MainController.java
@@ -0,0 +1,42 @@
+package shoppingMall.mainController;
+
+public class MainController {
+
+	// variable
+	private static UserController userController;
+	private static ProductController productController;
+	private static OrderController orderController;
+	private static LoginController loginController;
+	private static ArticleController articleController;
+	
+	public MainController(){
+		
+		this.userController = new UserController();
+		this.productController = new ProductController();
+		this.orderController = new OrderController();
+		this.loginController = new LoginController();
+		this.articleController = new ArticleController();
+		
+	}
+
+	public static UserController getUserController() {
+		return userController;
+	}
+
+	public static ProductController getProductController() {
+		return productController;
+	}
+
+	public static OrderController getOrderController() {
+		return orderController;
+	}
+
+	public static LoginController getLoginController() {
+		return loginController;
+	}
+
+	public static ArticleController getArticleController() {
+		return articleController;
+	}
+	
+}
