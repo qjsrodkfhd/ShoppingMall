9f27a2965e3239f184cf21c152bb9d67e40b06af
choiwj1012 <choiwj1012@hanmail.net>
2016-08-26 09:59:43 +0900

    update mainMenu

--- src/shoppingMall/main/Main.java
+++ src/shoppingMall/main/Main.java
@@ -1,10 +1,13 @@
 package shoppingMall.main;
 
+import shoppingMall.mainView.MainView;
+
 public class Main {
 	
 	public static void main(String[] args) {
 		
-		
+		MainView mainView = new MainView();
+		mainView.mainView();
 		
 	}
 	
