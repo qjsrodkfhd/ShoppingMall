000e10e467e5125f87a1be7367daf0a4997037c3
choiwj1012 <choiwj1012@hanmail.net>
2016-08-26 12:03:21 +0900

    update main

--- src/shoppingMall/main/Main.java
+++ src/shoppingMall/main/Main.java
@@ -1,11 +1,13 @@
 package shoppingMall.main;
 
+import shoppingMall.mainController.MainController;
 import shoppingMall.mainView.MainView;
 
 public class Main {
 	
 	public static void main(String[] args) {
 		
+		new MainController();
 		MainView mainView = new MainView();
 		mainView.mainView();
 		
--- src/shoppingMall/mainController/MainController.java
+++ src/shoppingMall/mainController/MainController.java
@@ -1,46 +1,44 @@
 package shoppingMall.mainController;
 
-import shoppingMall.mainView.MainView;
-
 public class MainController {
 
 	// variable
-	// private static UserController userController;
-	// private static ProductController productController;
-	// private static OrderController orderController;
-	// private static LoginController loginController;
-	// private static ArticleController articleController;	
-	
+	private static UserController userController;
+	private static ProductController productController;
+	private static OrderController orderController;
+	private static LoginController loginController;
+	private static ArticleController articleController;	
+
 	// constructor
 	public MainController(){
-		
-//		this.userController = new UserController();
-//		this.productController = new ProductController();
-//		this.orderController = new OrderController();
-//		this.loginController = new LoginController();
-//		this.articleController = new ArticleController();
-		
+
+		userController = new UserController();
+		productController = new ProductController();
+		orderController = new OrderController();
+		loginController = new LoginController();
+		articleController = new ArticleController();
+
 	}
 
-	// getter and setter
-//	public static UserController getUserController() {
-//		return userController;
-//	}
-//
-//	public static ProductController getProductController() {
-//		return productController;
-//	}
-//
-//	public static OrderController getOrderController() {
-//		return orderController;
-//	}
-//
-//	public static LoginController getLoginController() {
-//		return loginController;
-//	}
-//
-//	public static ArticleController getArticleController() {
-//		return articleController;
-//	}
-	
+	//getter and setter
+	public static UserController getUserController() {
+		return userController;
+	}
+
+	public static ProductController getProductController() {
+		return productController;
+	}
+
+	public static OrderController getOrderController() {
+		return orderController;
+	}
+
+	public static LoginController getLoginController() {
+		return loginController;
+	}
+
+	public static ArticleController getArticleController() {
+		return articleController;
+	}
+
 }
