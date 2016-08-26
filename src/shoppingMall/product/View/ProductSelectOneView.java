64985ab8334b1a2a5c60c0fbacfade561a8dc5fc
webtohouse <webtohouse@gmail.com>
2016-08-26 20:57:26 +0900

    업데이트

--- src/shoppingMall/product/View/ProductSelectOneView.java
+++ src/shoppingMall/product/View/ProductSelectOneView.java
@@ -1,9 +1,21 @@
 package shoppingMall.product.View;
 
+import shoppingMall.product.vo.Product;
+
 public class ProductSelectOneView {
-	
-	public void selectOneProduct() {
+
+	public void selectOneProduct(Product searchProduct) {
+
+		System.out.println("---------------------------------------");
+		System.out.println("상품번호\t상품이름\t상품가격\t상품브랜드\t상품색상");
+
+		System.out.print(listAll.get(i).getProductNumber() + "\t"); //상품 번호
+		System.out.print(listAll.get(i).getProductName() + "\t"); //상품 이름
+		System.out.print(listAll.get(i).getProductPrice() + "\t"); //상품 가격
+		System.out.print(listAll.get(i).getProductBrandName() + "\t"); //상품 브랜드
+		System.out.println(listAll.get(i).getProductColor()); // 상품 색상
 		
+		System.out.println("---------------------------------------");
 	}
 
 }
