18290c49f179db98373650f66e7f9b483b7eddb9
choiwj1012 <choiwj1012@hanmail.net>
2016-08-26 12:15:55 +0900

    fixed error

--- src/shoppingMall/mainController/MainController.java
+++ src/shoppingMall/mainController/MainController.java
@@ -1,102 +1,33 @@
 package shoppingMall.mainController;
 
-<<<<<<< HEAD
+import shoppingMall.Login.Controller.LoginController;
+import shoppingMall.User.Controller.UserController;
+
 public class MainController {
 
 	// variable
 	private static UserController userController;
-	private static ProductController productController;
-	private static OrderController orderController;
+	//private static ProductController productController;
+	//private static OrderController orderController;
 	private static LoginController loginController;
-	private static ArticleController articleController;	
+	//private static ArticleController articleController;	
 
 	// constructor
 	public MainController(){
 
 		userController = new UserController();
-		productController = new ProductController();
-		orderController = new OrderController();
+		//productController = new ProductController();
+		//orderController = new OrderController();
 		loginController = new LoginController();
-		articleController = new ArticleController();
+		//articleController = new ArticleController();
 
-	}
-
-	//getter and setter
-	public static UserController getUserController() {
-		return userController;
-	}
-
-	public static ProductController getProductController() {
-		return productController;
-	}
-
-	public static OrderController getOrderController() {
-		return orderController;
-	}
-
-	public static LoginController getLoginController() {
-		return loginController;
-	}
-
-	public static ArticleController getArticleController() {
-		return articleController;
-	}
-
-=======
-<<<<<<< HEAD
-import shoppingMall.Login.Controller.LoginController;
-import shoppingMall.User.Controller.UserController;
-=======
-import shoppingMall.mainView.MainView;
->>>>>>> refs/remotes/choiwj1012/master
-
-public class MainController {
-
-	// variable
-<<<<<<< HEAD
-	 private static UserController userController;
-	// private static ProductController productController;
-	// private static OrderController orderController;
-	 private static LoginController loginController;
-=======
-	// private static UserController userController;
-	// private static ProductController productController;
-	// private static OrderController orderController;
-	// private static LoginController loginController;
->>>>>>> refs/remotes/choiwj1012/master
-	// private static ArticleController articleController;	
-	
-	// constructor
-	public MainController(){
-		
-<<<<<<< HEAD
-		userController = new UserController();
-//		productController = new ProductController();
-//		orderController = new OrderController();
-		loginController = new LoginController();
-//		articleController = new ArticleController();
-=======
-//		this.userController = new UserController();
-//		this.productController = new ProductController();
-//		this.orderController = new OrderController();
-//		this.loginController = new LoginController();
-//		this.articleController = new ArticleController();
->>>>>>> refs/remotes/choiwj1012/master
-		
 	}
 
 	// getter and setter
-<<<<<<< HEAD
 	public static UserController getUserController() {
 		return userController;
-
 	}
-=======
-//	public static UserController getUserController() {
-//		return userController;
-//	}
->>>>>>> refs/remotes/choiwj1012/master
-//
+
 //	public static ProductController getProductController() {
 //		return productController;
 //	}
@@ -104,20 +35,13 @@ public class MainController {
 //	public static OrderController getOrderController() {
 //		return orderController;
 //	}
-//
-<<<<<<< HEAD
+
 	public static LoginController getLoginController() {
 		return loginController;
 	}
-=======
-//	public static LoginController getLoginController() {
-//		return loginController;
-//	}
->>>>>>> refs/remotes/choiwj1012/master
-//
+
 //	public static ArticleController getArticleController() {
 //		return articleController;
 //	}
-	
->>>>>>> origin/master
+
 }
