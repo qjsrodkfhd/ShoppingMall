8b48b7369ba647fb4e77722b2f31a8f83d980007
haneul123 <ww4020@naver.com>
2016-08-26 16:09:52 +0900

    update alertView and userdao

--- src/shoppingMall/User/Dao/UserDao.java
+++ src/shoppingMall/User/Dao/UserDao.java
@@ -8,46 +8,63 @@ import shoppingMall.User.Repository.UserRepository;
 import shoppingMall.User.Vo.User;
 
 public class UserDao {
-	
-	
+
+
 	public UserDao(){
-		
+
 		new UserRepository();
-		
+
 	}
 
-	
+
 	//가입
 	public boolean userSignUp(User user){
-		
+
 		boolean success = false;
-		
+
 		int currentUserNumber = UserRepository.getLastUserPosition();
 		currentUserNumber = currentUserNumber++;
 		UserRepository.setLastUserPosition(currentUserNumber);
-		
+
 		user.setUserNumber(currentUserNumber);
-		
+
 		ArrayList<User> currentUsers = UserRepository.getUsers();
 		currentUsers.add(user);
-		
+
 		success = true;
-		
+
 		return success;
-			
+
 	}
 
 	//목록
 	public ArrayList<User> userList(){
-		
+
 		ArrayList<User> userList;
-		
+
 		userList = UserRepository.getUsers();
-		
+
 		return userList;
-		
+
 	}
 
-	
-	
+
+	public boolean searchUser(int searchNumber){
+
+		boolean success = false;
+
+		User user;
+
+		for(int i=0; i<UserRepository.getUsers().size(); i++){
+			if(searchNumber == UserRepository.getUsers().get(i).getUserNumber()){
+				user=UserRepository.getUsers().get(i);
+				break;
+
+			}
+		}
+
+		return success;
+
+	}
+
 }
